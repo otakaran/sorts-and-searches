68741628cce10a27c281304020d63eb350d5a3d5
Otakar Andrysek <otakar@me.com>
2017-04-01 22:45:54 -0700

    Added code outline

---
+++ Sort.java
@@ -0,0 +1,71 @@
+
+/**
+ * Sort contain a number of methods that sort a file of comma seperated integers into correct
+ * using different algorithms.
+ * 
+ * @author Otakar Andrysek
+ * @version .1
+ * @date 4/1/2017
+ */
+public class Sort
+{
+    // Instance variables
+    private int x;
+
+    /**
+     * Constructor for objects of class Sort
+     */
+    public Sort()
+    {
+        // Initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int insertSort()
+    {
+        
+        return 0;
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int mergeSort()
+    {
+        
+        return 0;
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int selectionSort()
+    {
+        
+        return 0;
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int main()
+    {
+        
+        return 0;
+    }
+}
